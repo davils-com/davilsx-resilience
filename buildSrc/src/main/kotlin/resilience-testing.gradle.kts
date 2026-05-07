ca1490e65ddb669e22f495e749419fdca825040e
Nils Jaekel <nils.jaekel@davils.com>
2026-05-07 20:24:12 +0200

    feat: add resilience-testing.gradle.kts for test configuration and reporting

---
+++ buildSrc/src/main/kotlin/resilience-testing.gradle.kts
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2026 Davils
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    org.jetbrains.kotlin.multiplatform
+    com.google.devtools.ksp
+    com.davils.kreate
+    io.kotest
+}
+
+kreate {
+    project {
+        tests {
+            enabled = true
+            maxParallelForks = Runtime.getRuntime().availableProcessors()
+            timeoutMinutes = 10L
+            ignoreFailures = false
+            alwaysRunTests = false
+            failOnNoDiscoveredTests = false
+
+            logging {
+                logPassedTests = true
+                logSkippedTests = true
+                logTestStarted = true
+            }
+
+            report {
+                enabled = true
+                xml = true
+                html = true
+            }
+        }
+    }
+}
