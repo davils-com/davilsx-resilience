7c788c8fa32c06561e7805c8077c75eb2ea05935
Nils Jaekel <nils.jaekel@davils.com>
2026-05-19 15:44:13 +0200

    feat: add build configuration for davilsx-resilience-common module

---
+++ davilsx-resilience-common/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    `resilience-core`
+    `resilience-code-analysis`
+    `resilience-compliance-security`
+    `resilience-testing`
+}
+
+kreate {
+    project {
+        name = "davilsx-resilience-common"
+        description = "A library for common utilities and abstractions used across resilience modules."
+    }
+}
+
+kotlin {
+    android {
+        namespace = "com.davils.resilience.common"
+    }
+
+    sourceSets {
+        androidHostTest {
+            dependencies {
+                implementation(libs.bundles.resilience.tests.common.impl)
+                implementation(libs.bundles.resilience.tests.jvm.impl)
+            }
+        }
+
+        commonMain {
+            dependencies {
+                implementation(libs.bundles.resilience.shared.impl)
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(libs.bundles.resilience.tests.common.impl)
+            }
+        }
+
+        jvmTest {
+            dependencies {
+                implementation(libs.bundles.resilience.tests.jvm.impl)
+            }
+        }
+    }
+}
