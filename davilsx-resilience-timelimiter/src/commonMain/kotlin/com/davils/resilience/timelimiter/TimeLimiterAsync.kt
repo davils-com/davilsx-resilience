3d0b26f2852a2674c108eac8c35ae245c28e059a
David Ernst <david.ernst@davils.com>
2026-05-24 14:05:33 +0200

    refactor: make TimeLimiterAsync class non-generic and update method signatures for type consistency

--- davilsx-resilience-timelimiter/src/commonMain/kotlin/com/davils/resilience/timelimiter/TimeLimiterAsync.kt
+++ davilsx-resilience-timelimiter/src/commonMain/kotlin/com/davils/resilience/timelimiter/TimeLimiterAsync.kt
@@ -12,10 +12,10 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
 
-public class TimeLimiterAsync<T>(private val data: TimeLimiterData<T>) : DisposableAsync {
+public class TimeLimiterAsync(private val data: TimeLimiterData) : DisposableAsync {
     private val detachedScope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 
-    public suspend fun execute(block: suspend () -> T): T? {
+    public suspend fun <T>execute(block: suspend () -> T): T? {
         if (data.timeout == Duration.ZERO) return block()
 
         return when (data.strategy) {
@@ -24,7 +24,7 @@ public class TimeLimiterAsync<T>(private val data: TimeLimiterData<T>) : Disposa
         }
     }
 
-    private suspend fun executeHard(block: suspend () -> T): T? {
+    private suspend fun <T>executeHard(block: suspend () -> T): T? {
         try {
             return withTimeout(data.timeout.inWholeMilliseconds.milliseconds) {
                 block()
@@ -35,7 +35,7 @@ public class TimeLimiterAsync<T>(private val data: TimeLimiterData<T>) : Disposa
         }
     }
 
-    private suspend fun executeSoft(block: suspend () -> T): T? {
+    private suspend fun <T>executeSoft(block: suspend () -> T): T? {
         val deferred = detachedScope.async {
             block()
         }
@@ -53,8 +53,9 @@ public class TimeLimiterAsync<T>(private val data: TimeLimiterData<T>) : Disposa
         }
     }
 
-    private suspend fun handleFallbackOrThrow(exception: Throwable): T? {
-        val fallback = data.fallback
+    @Suppress("UNCHECKED_CAST")
+    private suspend fun <T>handleFallbackOrThrow(exception: Throwable): T? {
+        val fallback = data.fallback as? (suspend (Throwable) -> T?)
         return if (fallback != null) {
             try {
                 fallback(exception)
@@ -71,4 +72,5 @@ public class TimeLimiterAsync<T>(private val data: TimeLimiterData<T>) : Disposa
     }
 }
 
-public fun <T> timeLimiter(builder: TimeLimiterBuilder<T>.() -> Unit): TimeLimiterAsync<T> = TimeLimiterAsync<T>(TimeLimiterBuilder<T>().apply(builder).build())
+public fun <T> timeLimiter(builder: TimeLimiterBuilder.() -> Unit): TimeLimiterAsync =
+    TimeLimiterAsync(TimeLimiterBuilder().apply(builder).build())
