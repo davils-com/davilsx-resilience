70200389b097a3e328b92f637f49172626ef686a
David Ernst <david.ernst@davils.com>
2026-05-24 14:05:51 +0200

    refactor: remove generic type from TimeLimiterRegistry and update registry type

--- davilsx-resilience-timelimiter/src/commonMain/kotlin/com/davils/resilience/timelimiter/TimeLimiterRegistry.kt
+++ davilsx-resilience-timelimiter/src/commonMain/kotlin/com/davils/resilience/timelimiter/TimeLimiterRegistry.kt
@@ -2,4 +2,4 @@ package com.davils.resilience.timelimiter
 
 import com.davils.resilience.common.registry.Registry
 
-public class TimeLimiterRegistry<T> : Registry<TimeLimiterAsync<T>>()
+public class TimeLimiterRegistry : Registry<TimeLimiterAsync>()
