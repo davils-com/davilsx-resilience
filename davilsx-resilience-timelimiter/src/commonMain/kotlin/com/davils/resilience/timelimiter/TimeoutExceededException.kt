03307a6c1d7940c4d40a61807b05a8227c173b59
David Ernst <david.ernst@davils.com>
2026-05-24 14:05:57 +0200

    refactor: format TimeoutExceededException constructor for improved readability

--- davilsx-resilience-timelimiter/src/commonMain/kotlin/com/davils/resilience/timelimiter/TimeoutExceededException.kt
+++ davilsx-resilience-timelimiter/src/commonMain/kotlin/com/davils/resilience/timelimiter/TimeoutExceededException.kt
@@ -1,4 +1,5 @@
 package com.davils.resilience.timelimiter
 
-public class TimeoutExceededException(public val timeoutMs: Long, message: String? = null) : RuntimeException(message ?: "Execution exceeded timeout of ${timeoutMs}ms")
+public class TimeoutExceededException(timeoutMs: Long, message: String? = null) :
+    RuntimeException(message ?: "Execution exceeded timeout of ${timeoutMs}ms")
 
