f7ecb4f79f0af904687834c47d9da81209a9eb70
Nils Jaekel <nils.jaekel@davils.com>
2026-05-27 11:41:51 +0200

    feat: add `resilience-compliance-security` plugin and configure kreate project metadata, along with detekt setup for code analysis

--- davilsx-resilience-micrometer/build.gradle.kts
+++ davilsx-resilience-micrometer/build.gradle.kts
@@ -2,6 +2,14 @@ plugins {
     `resilience-common-jvm`
     `resilience-core-jvm`
     `resilience-testing-jvm`
+    `resilience-compliance-security`
+}
+
+kreate {
+    project {
+        name = "davilsx-resilience-micrometer"
+        description = "A library for integrating resilience patterns with Micrometer metrics to provide insights into the performance and reliability of operations."
+    }
 }
 
 dependencies {
