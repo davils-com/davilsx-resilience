43bee7f6c31259ad0c598ce30df73702f5fdc916
Nils Jaekel <nils.jaekel@davils.com>
2026-05-28 10:12:05 +0200

    feat: add ResilienceRegistryEvent sealed class to define registry lifecycle and modification events

---
+++ davilsx-resilience-common/src/commonMain/kotlin/com/davils/resilience/common/registry/ResilienceRegistryEvent.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2026 Davils
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.davils.resilience.common.registry
+
+import com.davils.kore.pattern.reactive.event.EventMarker
+import com.davils.resilience.common.ResilienceComponent
+
+/**
+ * Base class for all events emitted by a [ResilienceRegistry].
+ *
+ * These events provide notifications about lifecycle changes and modifications
+ * within the registry, such as adding or removing components.
+ *
+ * @since 1.0.0
+ */
+public sealed class ResilienceRegistryEvent : EventMarker() {
+
+    /**
+     * Event emitted when a new component is added to the registry.
+     *
+     * @param name The name of the added component.
+     * @param component The component instance that was added.
+     * @since 1.0.0
+     */
+    public data class ItemAdded<C : ResilienceComponent<*, *>>(
+        public val name: String,
+        public val component: C
+    ) : ResilienceRegistryEvent()
+
+    /**
+     * Event emitted when a component is removed from the registry.
+     *
+     * @param name The name of the removed component.
+     * @param component The component instance that was removed.
+     * @since 1.0.0
+     */
+    public data class ItemRemoved<C : ResilienceComponent<*, *>>(
+        public val name: String,
+        public val component: C
+    ) : ResilienceRegistryEvent()
+
+    /**
+     * Event emitted when the registry is cleared.
+     *
+     * @since 1.0.0
+     */
+    public data object RegistryCleared : ResilienceRegistryEvent()
+
+    /**
+     * Event emitted when the registry is disposed.
+     *
+     * @since 1.0.0
+     */
+    public data object RegistryDisposed : ResilienceRegistryEvent()
+}
