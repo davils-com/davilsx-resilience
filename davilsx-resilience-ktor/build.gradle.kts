974caf4447439f67155e1b6c913e0892d04473c6
Nils Jaekel <nils.jaekel@davils.com>
2026-07-06 09:12:14 +0200

    chore: update dependencies for davilsx-resilience-ktor module and remove unused ktor-server-core reference

--- davilsx-resilience-ktor/build.gradle.kts
+++ davilsx-resilience-ktor/build.gradle.kts
@@ -10,18 +10,15 @@ kreate {
     project {
         name = "davilsx-resilience-ktor"
         description = "Ktor integration for davilsx-resilience components."
-        platform {
-            javaVersion = JavaVersion.toVersion(23)
-        }
     }
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:${libs.versions.ktor.get()}")
+    implementation(libs.bundles.resilience.ktor.common.impl)
     implementation(project(":davilsx-resilience-ratelimiter"))
     implementation(project(":davilsx-resilience-timelimiter"))
 
-    testImplementation("io.ktor:ktor-server-test-host:${libs.versions.ktor.get()}")
+    testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.bundles.resilience.tests.common.impl)
     testImplementation(libs.bundles.resilience.tests.jvm.impl)
 }
